d85cf8680558b1b4cd83734efbefb39591a7f142
Reinvy <satriaoner@gmail.com>
2023-02-04 23:36:05 +0700

    init project

---
+++ src/test/java/backend/inkwell/InkwellApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.inkwell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InkwellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
